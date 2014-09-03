6212fe6c158b71f5cd6fba04b17a29f011b41163
Wesley Sui <wesleysui@gmail.com>
2014-09-03 12:00:05 +0800

    first commit

---
+++ MarketManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarketManager
+//
+//  Created by Wesley Sui on 14-9-3.
+//  Copyright (c) 2014年 Jing Yun Sui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MarketManagerTests/MarketManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  MarketManagerTests.swift
+//  MarketManagerTests
+//
+//  Created by Wesley Sui on 14-9-3.
+//  Copyright (c) 2014年 Jing Yun Sui. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MarketManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
